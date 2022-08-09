1015b264641c1233fb7907f356fa7d2fdad127f1
nsalto <nahuelsalto.71@gmail.com>
2022-08-09 02:13:09 -0300

    dbError

--- src/utils/Error.ts
+++ src/utils/Error.ts
@@ -1,39 +1,47 @@
 export class Error {
-    type: string;
-    code: number;
-    message: string;
-  
-    constructor(type: string, message: string, code: number) {
-      this.type = type;
-      this.message = message;
-      this.code = code;
-    }
-  
-    static badRequest(message: string) {
-      const error = {
-        type: "Bad Request",
-        message: message,
-        code: 400,
-      };
-      return new Error(error.type, error.message, error.code);
-    }
-  
-    static invalidHash(message: string) {
-      const error = {
-        type: "Invalid Hash",
-        message: message,
-        code: 400,
-      };
-      return new Error(error.type, error.message, error.code);
-    }
-  
-    static internalServer(message: string) {
-      const error = {
-        type: "Internal Server Error!",
-        message: message,
-        code: 500,
-      };
-  
-      return new Error(error.type, error.message, error.code);
-    }
+  type: string;
+  code: number;
+  message: string;
+
+  constructor(type: string, message: string, code: number) {
+    this.type = type;
+    this.message = message;
+    this.code = code;
   }
+
+  static badRequest(message: string) {
+    const error = {
+      type: "Bad Request",
+      message: message,
+      code: 400,
+    };
+    return new Error(error.type, error.message, error.code);
+  }
+
+  static invalidHash(message: string) {
+    const error = {
+      type: "Invalid Hash",
+      message: message,
+      code: 400,
+    };
+    return new Error(error.type, error.message, error.code);
+  }
+
+  static internalServer(message: string) {
+    const error = {
+      type: "Internal Server Error!",
+      message: message,
+      code: 500,
+    };
+    return new Error(error.type, error.message, error.code);
+  }
+
+  static dbError(message:string) {
+    const error = {
+      type: "dbError",
+      message: message,
+      code: 500,
+    };
+    return new Error(error.type, error.message, error.code);
+  }
+}
